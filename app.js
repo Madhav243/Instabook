da2676eb59978001e0127f3ee1d75bf1d7fcec03
Madhav243 <madhav.verma108@gmail.com>
2021-01-05 18:19:36 +0530

    add initial files

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+    res.render("index")
 
+
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
